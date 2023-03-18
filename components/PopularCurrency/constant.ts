f20c5fbe4a195e7bb676d4ab64bd2442f24843a5
Trung Nguyen <trung1704ptit@gmail.com>
2023-03-18 11:38:14 +0800

    add popurlar list

---
+++ components/PopularCurrency/constant.ts
@@ -0,0 +1,86 @@
+export const biggestEconomics = [
+  {
+    ISOCode: 'USD',
+    name: 'United States',
+  },
+  {
+    ISOCode: 'CNY',
+    name: 'China',
+  },
+  {
+    ISOCode: 'JPY',
+    name: 'Japan',
+  },
+  {
+    ISOCode: 'EUR',
+    name: 'Germany',
+  },
+  {
+    ISOCode: 'GBP',
+    name: 'The United Kingdom',
+  },
+  {
+    ISOCode: 'INR',
+    name: 'India',
+  },
+  {
+    ISOCode: 'CAD',
+    name: 'Canada',
+  },
+  {
+    ISOCode: 'KRW',
+    name: 'South Korea',
+  },
+  {
+    ISOCode: 'RUB',
+    name: 'Russia',
+  },
+  {
+    ISOCode: 'BRL',
+    name: 'Brazil',
+  },
+  {
+    ISOCode: 'AUD',
+    name: 'Australia',
+  },
+  {
+    ISOCode: 'MXN',
+    name: 'Mexico',
+  },
+  {
+    ISOCode: 'IDR',
+    name: 'Indonesia',
+  },
+  {
+    ISOCode: 'SGD',
+    name: 'Singapore',
+  },
+  {
+    ISOCode: 'SAR',
+    name: 'Saudi Arabia',
+  },
+  {
+    ISOCode: 'TRY',
+    name: 'Turkey',
+  },
+  {
+    ISOCode: 'CHF',
+    name: 'Switzerland',
+  },
+  {
+    ISOCode: 'PLN',
+    name: 'Poland',
+  },
+  {
+    ISOCode: 'SEK',
+    name: 'Sweden',
+  },
+  {
+    ISOCode: 'THB',
+    name: 'Thailand',
+  },
+  {
+    ISOCode: 'AED',
+    name: 'United Arab Emirates',
+  },
+];
--- components/PopularCurrency/index.tsx
+++ components/PopularCurrency/index.tsx
@@ -1,19 +1,27 @@
 import { Row, Col } from 'antd';
 import { Typography } from 'antd';
 import Container from '../Container';
+import { biggestEconomics } from './constant';
 import CurrencyBlock from './CurrencyBlock';
 import { PopularCurrencyTitle } from './style';
 
 const { Title } = Typography;
 
 export const PopularCurrency = ({ popularData }) => {
+  
+  const filterList = popularData?.filter(item => {
+    const filterItem = biggestEconomics.find(big => big.ISOCode === item.to);
+    if (filterItem) return true;
+    return false;
+  })
+
   return (
     <Container>
       <PopularCurrencyTitle>
         <Title level={2}>Popular Currency Pairings</Title>
       </PopularCurrencyTitle>
       <Row gutter={[16, 16]}>
-        {popularData?.map((item) => (
+        {filterList?.map((item) => (
           <Col xs={24} sm={12} md={8} lg={6} key={item.pairName}>
             <CurrencyBlock currencyData={item} />
           </Col>
